091baecef6322e5fc8674ccb399cfd174853c6f1
arribass <adri47arribas@gmail.com>
2020-06-20 01:01:36 +0200

    lint fixes

--- Basics_1.py
+++ Basics_1.py
@@ -1,8 +1,8 @@
 ' Author Adrian Arribas'
-#Math commands
-#abs(value), log(value), log10(value), max(val1, val2),
-#min(val1, val2), round(value), sin(value), sqrt(value) --> similar to R
-#constant --> e and pi
+# Math commands
+# abs(value), log(value), log10(value), max(val1, val2),
+# min(val1, val2), round(value), sin(value), sqrt(value) --> similar to R
+# constant --> e and pi
 import math
 x = -1
 y = 2
@@ -10,8 +10,8 @@ mi_valor_pi = 3.14
 ang1 = 90
 number = 64
 print(math.fabs(x))
-print(math.log(2,2))
-##f strings only work on python3
+print(math.log(2, 2))
+# f strings only work on python3
 print(f"The minvalue is : {min(x,y)}")
 print(f"The value of pi rounded is  : {round(mi_valor_pi)}")
 print(f"The sinus of the angle is : {math.sin(ang1)}")
